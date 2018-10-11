cb80f9b6b964eece187a6034b15dba42e16461fd
ch00209@163.com <ch00209@163.com>
2018-10-11 11:23:42 +0800

    变更

--- chtool/src/main/java/ch/chtool/base/BaseActivity.java
+++ chtool/src/main/java/ch/chtool/base/BaseActivity.java
@@ -44,7 +44,6 @@ public abstract class BaseActivity extends Activity {
         super.onCreate(savedInstanceState);
         //设置布局
         setContentView(intiLayout());
-        createLoadingDialog(BaseActivity.this, "加载中....");
         //初始化控件
         initView();
         //设置数据
@@ -138,43 +137,6 @@ public abstract class BaseActivity extends Activity {
         }
         startActivity(intent);
     }
-
-    private Dialog loadingDialog;
-
-    public Dialog createLoadingDialog(Context context, String msg) {
-        LayoutInflater inflater = LayoutInflater.from(context);
-        View v = inflater.inflate(R.layout.dialog_loading, null);// 得到加载view
-        LinearLayout layout = (LinearLayout) v.findViewById(R.id.dialog_loading_view);// 加载布局
-        TextView tipTextView = (TextView) v.findViewById(R.id.tipTextView);// 提示文字
-        tipTextView.setText(msg);// 设置加载信息
-
-        loadingDialog = new Dialog(context, R.style.MyDialogStyle);// 创建自定义样式dialog
-        loadingDialog.setCancelable(true); // 是否可以按“返回键”消失
-        loadingDialog.setCanceledOnTouchOutside(false); // 点击加载框以外的区域
-        loadingDialog.setContentView(layout, new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.MATCH_PARENT));// 设置布局
-        /**
-         *将显示Dialog的方法封装在这里面
-         */
-        Window window = loadingDialog.getWindow();
-        WindowManager.LayoutParams lp = window.getAttributes();
-        lp.width = WindowManager.LayoutParams.MATCH_PARENT;
-        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
-        window.setGravity(Gravity.CENTER);
-        window.setAttributes(lp);
-        window.setWindowAnimations(R.style.PopWindowAnimStyle);
-        loadingDialog.show();
-
-        return loadingDialog;
-    }
-
-    /**
-     * 关闭dialog
-     *
-     * @param mDialogUtils
-     */
-    public abstract void closeDialog(Dialog mDialogUtils);
 
 
 }
